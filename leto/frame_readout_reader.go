a5bb689252db0276d02b7a42d30c67e993b248ac
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-05-10 23:06:39 +0200

    Adds a RemoteManager

--- leto/frame_readout_reader.go
+++ leto/frame_readout_reader.go
@@ -9,7 +9,7 @@ import (
 
 func FrameReadoutReadAll(stream io.Reader, C chan<- *hermes.FrameReadout, E chan<- error) {
 	defer func() {
-		close(C)
+		//Do not close C, it is shared by many connections, its a global
 		close(E)
 	}()
 
---
+++ leto/remote_manager.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"net"
+	"sync"
+
+	"github.com/formicidae-tracker/hermes"
+)
+
+type RemoteManager struct {
+	mx          sync.Mutex
+	quit        chan struct{}
+	connections []net.Conn
+	listener    net.Listener
+}
+
+func NewRemoteManager() *RemoteManager {
+	return &RemoteManager{}
+}
+
+func (m *RemoteManager) Stop() {
+	m.mx.Lock()
+	defer m.mx.Unlock()
+	if m.quit != nil {
+		close(m.quit)
+	}
+	if m.listener != nil {
+		m.listener.Close()
+	}
+	for i, c := range m.connections {
+		if c == nil {
+			continue
+		}
+		c.Close()
+		m.connections[i] = nil
+	}
+}
+
+func (m *RemoteManager) Listen(address string, readouts chan<- *hermes.FrameReadout) {
+	wg := sync.WaitGroup{}
+
+	defer func() {
+		wg.Wait
+		close(readouts)
+	}()
+
+	m.mx.Lock()
+	var err error
+	m.listener, err := net.Listen("tcp",address)
+	if err != nil {
+		m.nx.Unlock()
+		m.listener = nil
+		return
+	}
+	m.quit = make(chan struct{})
+	m.mx.Unlock()
+
+	for {
+		conn, err := m.listener.Accept()
+		if err != nil {
+			select {
+				case <- m.quit:
+				return
+			default:
+				log.Printf("accept: %s",err)
+				continue
+			}
+		}
+
+		m.mx.Lock()
+		errors := make(chan error)
+		go func(remote string) {
+			for e := range errors {
+				log.Printf("[remote/%s]: %s",remote,err)
+			}
+		}(conn.RemoteAddr())
+		wg.Add(1)
+
+		m.connections = append(m.connections, conn)
+		go func(idx int) {
+			FrameReadoutReadAll(conn, readouts, errors)
+			m.mx.Lock()
+			defer m.mx.Unlock()
+			if m.connections[idx] != nil {
+				m.connections[idx] = nil
+			}
+			wg.Done()
+		}(len(m.connections)-1)
+		m.mx.Unlock()
+	}
+
+}
