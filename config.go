682e1ba651d4800f090c2755926e02408fe1ff35
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2020-09-04 12:35:24 +0200

    Switches to artemis v0.4.0

--- config.go
+++ config.go
@@ -5,4 +5,4 @@ const MINOR_FMT_VERSION int = 5
 const LETO_PORT int = 4000
 const ARTEMIS_IN_PORT int = 4001
 const ARTEMIS_OUT_PORT int = 4002
-const ARTEMIS_MIN_VERSION = "v0.3.0"
+const ARTEMIS_MIN_VERSION = "v0.4.0"
