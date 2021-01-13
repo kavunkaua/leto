69313e65daba5bcb025b12eb4e402c9cca3c24f1
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-01-13 17:21:33 +0100

    Bumps to version v0.4.2

--- config.go
+++ config.go
@@ -7,6 +7,6 @@ const MINOR_FMT_VERSION int = 5
 const LETO_PORT int = 4000
 const ARTEMIS_IN_PORT int = 4001
 const ARTEMIS_OUT_PORT int = 4002
-const LETO_VERSION = "v0.4.1"
+const LETO_VERSION = "v0.4.2"
 const ARTEMIS_MIN_VERSION = "v0.4.0"
 const NODE_CACHE_TTL = 5 * time.Second
